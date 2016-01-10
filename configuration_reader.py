7c52a83820ace9df7a212fc505ac488f5e5a3ee5
Satish Kumar <satvidh@gmail.com>
2016-01-09 20:23:42 -0600

    1: Implement a network with a cidr_block.
    Scenario 3: Render the template loaded from .sthapathi/templates/PROVIDER/RESOURCE
    ---------------------------------------------------------------------------------------------
    GIVEN user specifies a resource named RESOURCE
    AND the user specifies a provider named PROVIDER
    AND sthapathi finds the template for the resource under
    .sthapathi/templates/PROVIDER/RESOURCE
    AND sthapathi loads the mustache template from .sthapathi/templates/PROVIDER/RESOURCE
    WHEN sthapathi applies the parameters for the resource
    THEN sthapathi creates the code that generates RESOURCE on PROVIDER.
    
    test_configuration_reader.py, configuration_reader.py
    ---------------------------------------------------------
    * Add test for the configuration reader and make it pass.

--- configuration_reader.py
+++ configuration_reader.py
@@ -1,12 +1,16 @@
 class ConfigurationReader(object):
-    @staticmethod
-    def read_type(configuration):
-        pass
+    __TYPE_INDEX = 0
+    __NAME_INDEX = 1
+    __PARAMETERS_INDEX = 2
 
     @staticmethod
-    def read_name(configuration):
-        pass
+    def read_type(configuration_parameters):
+        return configuration_parameters[ConfigurationReader.__TYPE_INDEX]
 
     @staticmethod
-    def read_parameters(configuration):
-        pass
+    def read_name(configuration_parameters):
+        return configuration_parameters[ConfigurationReader.__NAME_INDEX]
+
+    @staticmethod
+    def read_parameters(configuration_parameters):
+        return configuration_parameters[ConfigurationReader.__PARAMETERS_INDEX]
---
+++ tests/test_configuration_reader.py
@@ -0,0 +1,22 @@
+import configuration_reader
+from nose.tools import assert_equal
+
+
+class TestConfigurationReader(object):
+    def __init__(self):
+        self._configuration_reader = None
+
+    def setup(self):
+        self._configuration_reader = configuration_reader.ConfigurationReader()
+
+    def test_configuration_reader(self):
+        expected_resource = 'RESOURCE'
+        expected_name = 'NAME'
+        expected_parameters = {'parameter': 'value'}
+        configuration_parameters = [expected_resource, expected_name, expected_parameters]
+        resource = self._configuration_reader.read_type(configuration_parameters)
+        name = self._configuration_reader.read_name(configuration_parameters)
+        parameters = self._configuration_reader.read_parameters(configuration_parameters)
+        assert_equal(expected_resource, resource)
+        assert_equal(expected_name, name)
+        assert_equal(expected_parameters, parameters)
